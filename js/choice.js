0e16ea9ff19e7412617723c1e95ea6b810f4e47b
Nicolas MICAUX <nmi@ivalua.com>
2024-04-02 11:07:54 +0200

    Fix path problems (bis)

--- js/choice.js
+++ js/choice.js
@@ -433,7 +433,7 @@ const pickDev = () => {
             const txtDevCurrent = document.getElementById('txtDevCurrent');
             txtDevCurrent.textContent = null;
             const img = document.createElement('img');
-            img.src = './assets/icons/fuck.png';
+            img.src = './images/fuck.png';
             img.style.width = '150px';
             txtDevCurrent.after(img);
             btn.disabled = true;
@@ -620,7 +620,7 @@ const initCauchyStatus = () => {
         if (s === true){
             icon = 'opened'
         }
-        img.setAttribute('src', `assets/icons/delorean-${icon}.png`);
+        img.setAttribute('src', `images/delorean-${icon}.png`);
         img.classList.remove('hidden')
     })
 }
