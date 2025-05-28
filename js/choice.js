a2b7628aee354da8daff3b573f53dd21b5b3cbe5
cde-ivalua <144218655+cde-ivalua@users.noreply.github.com>
2025-05-28 09:58:54 +0200

    Update choice.js

--- js/choice.js
+++ js/choice.js
@@ -23,8 +23,7 @@ const teams = [
         "trelloId": "NU3JyLTu",
         "devs":
         {
-            
-            'akli': {'trigram' : 'akli', 'name': 'Akli', 'present' : getBoolFromStorage('Akli'), 'tab' : null, 'done' : false, 'time' : 0 },
+            'akl': {'trigram' : 'akl', 'name': 'Akli', 'present' : getBoolFromStorage('Akli'), 'tab' : null, 'done' : false, 'time' : 0 },
             'cda': {'trigram' : 'cda', 'name': 'Christophe', 'trello': '5721daa1b7364971e546197a', 'present' : getBoolFromStorage('cda'), 'tab' : null, 'done' : false, 'time' : 0 },
             'cdn': {'trigram' : 'cdn', 'name': 'Christopher', 'present' : getBoolFromStorage('cdn'), 'tab' : null, 'done' : false, 'time' : 0 },
             'cdr': {'trigram' : 'cdr', 'name': 'Cyril', 'present' : getBoolFromStorage('cdr'), 'tab' : null, 'done' : false, 'time' : 0 },
